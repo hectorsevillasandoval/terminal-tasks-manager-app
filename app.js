6ac98caec294b8911c369f4ad7476b6ec3a1053c
Hector Sevilla <hsevilla@consultwebs-email.com>
2022-11-19 14:30:16 -0600

    Entry point of the application

---
+++ app.js
@@ -0,0 +1,16 @@
+const { showMenu, pause } = require('./helpers/messages');
+
+require( 'colors' );
+
+const main = async () => {
+    let opt = '';
+    
+    do{
+        opt = await showMenu();
+        console.log( { opt } );
+        if( opt !== '0' ) await pause();
+    } while( opt !== '0' );
+};
+
+
+main();
